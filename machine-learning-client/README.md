63d09acbb674ce77bcf4c328e45112995355ad36
noidthewanderer <nido_ove@outlook.com>
2023-12-02 21:51:10 -0500

    ml that analyzes a static base64 correctly

--- machine-learning-client/README.md
+++ machine-learning-client/README.md
@@ -4,7 +4,17 @@ docker build -t ml_client .
 <!-- docker build --no-cache -t ml_client . -->
 
 To run:
-docker run -it ml_client
+<!-- docker run -it ml_client -->
+docker run -it --rm --network=my_network ml_client
 
+restart db and run:
+docker stop mongodb
+docker rm mongodb
+docker run -d --network=my_network --name=mongodb mongo:latest
+docker run -it --rm --network=my_network ml_client
+
+
+web-app:
 docker-compose build
 docker-compose up
+
--- machine-learning-client/play.py
+++ machine-learning-client/play.py
@@ -1,7 +1,11 @@
 import cv2
 import mediapipe
+import random
 
-# --------- DB
+moves = ["Rock", "Paper", "Scissors"]
+wins = {"Rock": "Scissors", "Paper": "Rock", "Scissors": "Paper"}
+
+# ---------------- DB -----------------
 
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
@@ -25,20 +29,64 @@ if "mycollection" not in db.list_collection_names():
 
 collection = db["mycollection"]
 
-def insert_result_to_db(move):
-    collection.insert_one({'gesture': move})
+def insert_result_to_db(result):
+    collection.insert_one(result)
 
 def print_collection_contents():
     cursor = collection.find()
 
     print("Contents of the MongoDB collection:")
     for document in cursor:
-        print(document['gesture'])
+        # Use get() method to safely access the fields
+        player_gesture = document.get('playerGesture', 'N/A')
+        comp_gesture = document.get('compGesture', 'N/A')
+        winner = document.get('winner', 'N/A')
+        image_base64 = document.get('image', None)
+
+        print(f"Player Gesture: {player_gesture}, Comp Gesture: {comp_gesture}, Winner: {winner}")
 
 
-# --------- DB
+
+# ---------------- GAME ------------------
+
+import base64
+from io import BytesIO
+from PIL import Image
+import numpy as np
+
+def decode_photo_data_url(photo_data_url):
+    _, encoded_data = url.split(',', 1)
+
+    # Decode the Base64-encoded data
+    image_data = base64.b64decode(encoded_data)
+
+    image_array = np.frombuffer(image_data, dtype=np.uint8)
+    frame = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+    return frame
+
+# Example usage:
+
+# Now, 'decoded_image' contains the image in a format that OpenCV can handle
 
 
+# ---------------
+
+def get_comp_move():
+    comp = random.randint(0, 2)
+    return moves[comp]
+
+def calculate_game_state(comp, move):
+
+    if move not in wins:
+        return -1 # gesture not detected
+    
+    if comp == move:
+        return 0 # tie
+
+    if wins[move] == comp:
+        return 1 # player win
+
+    return 2 # computer win
 
 def get_finger_status(hands_module, hand_landmarks, finger_name):
     finger_id_map = {'INDEX': 8, 'MIDDLE': 12, 'RING': 16, 'PINKY': 20}
@@ -56,10 +104,11 @@ def get_thumb_status(hands_module, hand_landmarks):
     
     return thumb_tip_x > thumb_ip_x > thumb_mcp_x
 
-def analyze_image(image_path):
+def analyze_image(decoded_image):
     hands_module = mediapipe.solutions.hands
 
-    frame = cv2.imread(image_path)
+    # frame = cv2.imread(decoded_image)
+    frame = decoded_image
 
     with hands_module.Hands(static_image_mode=True, min_detection_confidence=0.7,
                             min_tracking_confidence=0.4, max_num_hands=2) as hands:
@@ -102,8 +151,29 @@ def analyze_image(image_path):
     return move
 
 if __name__ == "__main__":
-    image_path = "hand.png"
-    result = analyze_image(image_path)
-    insert_result_to_db(result)
-    print("Final recognized gesture:", result)
+    import json
+    with open('message.txt', 'r') as file:
+        file_content = file.read()
+
+    # Parse the JSON content
+    json_data = json.loads(file_content)
+
+    # Access and print the content of the 'PhotoUrl' key
+    photoBase64 = json_data["photoDataUrl"]
+
+
+    with open('url.txt', 'r') as file:
+        url = file.read()
+
+    decoded_image = decode_photo_data_url(url)
+    playerGesture = analyze_image(decoded_image)
+    compGesture = get_comp_move()
+    winner = calculate_game_state(compGesture, playerGesture)
+
+    insert_result_to_db({"playerGesture": playerGesture, 
+                         "compGesture": compGesture,
+                         "winner": winner,
+                         "image": url})
+
+    # print("Final recognized gesture:", playerGesture)
     print_collection_contents()
